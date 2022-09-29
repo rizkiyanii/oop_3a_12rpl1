168f8253ee15ad7aae1148d4dbc3728255ef7e11
Rizkiyani <rzkyn0275@gmail.com>
2022-09-28 16:21:31 -1200

    28-09-2022

---
+++ comment_1.php
@@ -0,0 +1,5 @@
+<?php
+echo "<p>Halo dunia</p>";
+echo "<p>saya belajar PHP</p>";
+echo "<p>ini program pertama saya!</p>";
+?>
---
+++ comment_2.php
@@ -0,0 +1,5 @@
+<?php
+echo "Saya belajar PHP\n";
+echo "Ini baris kedua\n";
+echo "Ini baris ketiga\n";
+?>
